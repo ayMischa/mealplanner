c20982a62dbd010700f963054b8e78ca5c670ff7
Michael Marusin <michaelmarusin@gmail.com>
2026-06-03 12:23:51 +0200

    Add connect and read timeouts to MealDB RestClient

--- src/main/java/de/dhbw/webeng/mealplanner/config/RestClientConfig.java
+++ src/main/java/de/dhbw/webeng/mealplanner/config/RestClientConfig.java
@@ -3,16 +3,27 @@ package de.dhbw.webeng.mealplanner.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
     public RestClient mealDbRestClient(
-            @Value("${mealdb.base-url}") String baseUrl) {
+            @Value("${mealdb.base-url}") String baseUrl,
+            @Value("${mealdb.connect-timeout-ms}") int connectTimeoutMs,
+            @Value("${mealdb.read-timeout-ms}") int readTimeoutMs) {
+
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofMillis(connectTimeoutMs));
+        factory.setReadTimeout(Duration.ofMillis(readTimeoutMs));
+
         return RestClient.builder()
                 .baseUrl(baseUrl)
+                .requestFactory(factory)
                 .build();
     }
 }
